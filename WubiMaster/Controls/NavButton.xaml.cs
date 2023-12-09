851ede49b25ca9cb788ecd8cc741e0b4b59dd667
空山明月 <mrshiqiqi@126.com>
2023-12-09 16:34:54 +0800

    update

---
+++ WubiMaster/Controls/NavButton.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WubiMaster.Controls
+{
+    public partial class NavButton : UserControl
+    {
+        public NavButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(NavButton));
+
+        public string IconText
+        {
+            get { return (string)GetValue(IconTextProperty); }
+            set { SetValue(IconTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconTextProperty =
+            DependencyProperty.Register("IconText", typeof(string), typeof(NavButton));
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(NavButton));
+
+        public Brush IconColor
+        {
+            get { return (Brush)GetValue(IconColorProperty); }
+            set { SetValue(IconColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconColorProperty =
+            DependencyProperty.Register("IconColor", typeof(Brush), typeof(NavButton));
+
+        public double TextFontSize
+        {
+            get { return (double)GetValue(TextFontSizeProperty); }
+            set { SetValue(TextFontSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextFontSizeProperty =
+            DependencyProperty.Register("TextFontSize", typeof(double), typeof(NavButton));
+
+        public double IcontFontSize
+        {
+            get { return (double)GetValue(IcontFontSizeProperty); }
+            set { SetValue(IcontFontSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty IcontFontSizeProperty =
+            DependencyProperty.Register("IcontFontSize", typeof(double), typeof(NavButton));
+
+        public Brush TextColor
+        {
+            get { return (Brush)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextColorProperty =
+            DependencyProperty.Register("TextColor", typeof(Brush), typeof(NavButton));
+    }
+}
---
+++ WubiMaster/Views/MainView.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WubiMaster.Views
+{
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            Application.Current.Resources.MergedDictionaries[0].Source = new Uri("pack://application:,,,/Themes/DarkYellowTheme.xaml");
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private bool isMaximized = false;
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (isMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1000;
+                    this.Height = 618;
+
+                    isMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    isMaximized = true;
+                }
+            }
+        }
+
+        private void btn_theme_Click(object sender, RoutedEventArgs e)
+        {
+            ResourceDictionary resource = new ResourceDictionary();
+            string darkYellowThemePack = "pack://application:,,,/Themes/DarkYellowTheme.xaml";
+            string darkBlueThemePack = "pack://application:,,,/Themes/DarkBlueTheme.xaml";
+            if (Application.Current.Resources.MergedDictionaries[0].Source.ToString() == darkYellowThemePack)
+            {
+                resource.Source = new Uri(darkBlueThemePack);
+            }
+            else
+            {
+                resource.Source = new Uri(darkYellowThemePack);
+            }
+            Application.Current.Resources.MergedDictionaries[0].Source = resource.Source;
+        }
+
+        private void btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
