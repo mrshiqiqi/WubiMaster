70e2dd7c516920baad334575c3b9e4671694a403
空山明月 <mrshiqiqi@126.com>
2024-01-07 00:50:53 +0800

    添加主题列表名称绑定源

--- WubiMaster/ViewModels/SettingsViewModel.cs
+++ WubiMaster/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows;
+using WubiMaster.Models;
 
 namespace WubiMaster.ViewModels
 {
-    class SettingsViewModel
+    public partial class SettingsViewModel : ObservableRecipient
     {
+        [ObservableProperty]
+        public ObservableCollection<ThemeModel> themeList;
+
+        public SettingsViewModel()
+        {
+            themeList = new ObservableCollection<ThemeModel>();
+            InitThemes();
+        }
+
+        [RelayCommand]
+        public void ChangeTheme(object obj)
+        {
+            if (obj == null) { return; }
+            MessageBox.Show(obj.ToString());
+        }
+
+        private void InitThemes()
+        {
+            List<string> themeNames = new List<string>()
+            {
+                "Gold Dark",
+                "Light Blue",
+                "Summer Meadow",
+                "Electric City Nights",
+                "Lavendar",
+                "Hacker News",
+                "Navy And Blush",
+                "White Width Blue",
+                "Dark Sage Green",
+                "Spring",
+                "Neon",
+            };
+
+            foreach (var themeName in themeNames)
+            {
+                ThemeModel themeModel = new ThemeModel();
+                themeModel.Name = themeName;
+                themeModel.Value = themeName;
+                ThemeList?.Add(themeModel);
+            }
+        }
     }
 }
