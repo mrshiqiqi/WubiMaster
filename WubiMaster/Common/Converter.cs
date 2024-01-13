5f8f784601c4bb9445b7495cee927ef49069aa4d
空山明月 <mrshiqiqi@126.com>
2024-01-14 01:43:58 +0800

    完成字根搜索控件设计

---
+++ WubiMaster/Common/Converter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WubiMaster.Common
+{
+    public class Converter
+    {
+    }
+
+    public class SearchTextToVisible : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return Visibility.Visible;
+            else if (value.ToString().Length == 0) return Visibility.Visible;
+            else return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return Visibility.Visible;
+            else if (value.ToString().Length == 0) return Visibility.Visible;
+            else return Visibility.Collapsed;
+
+        }
+    }
+}
--- WubiMaster/Controls/SearchTextbox.xaml.cs
+++ WubiMaster/Controls/SearchTextbox.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace WubiMaster.Controls
