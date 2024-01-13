fd1554eb6614d65c515903e39d8d0577a1602bd5
空山明月 <mrshiqiqi@126.com>
2024-01-14 03:10:18 +0800

    词库管理页面布局初步

---
+++ WubiMaster/Models/CikuModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WubiMaster.Models
+{
+    public class CikuModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Worlds { get; set; }
+        public string Group { get; set; }
+    }
+}
--- WubiMaster/ViewModels/LexiconViewModel.cs
+++ WubiMaster/ViewModels/LexiconViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WubiMaster.Models;
 
 namespace WubiMaster.ViewModels
 {
-    class LexiconViewModel
+    public partial class LexiconViewModel:ObservableObject
     {
+        [ObservableProperty]
+        public ObservableCollection<CikuModel> cikuList;
+
+        public LexiconViewModel()
+        {
+            ObservableCollection<CikuModel> temp = new ObservableCollection<CikuModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                CikuModel ckModel = new CikuModel();
+                ckModel.Id = i;
+                ckModel.Code = "hello";
+                ckModel.Worlds = "五笔大师";
+                ckModel.Group = "默认";
+                temp.Add(ckModel);
+            }
+
+            cikuList = temp;
+        }
     }
 }
