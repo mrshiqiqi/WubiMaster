f549d96e34d396dce1db41f29749bb4f34e9e226
shiqiqi <sqqlove123@163.com>
2024-01-16 14:13:15 +0800

    完成分布控件的设计

---
+++ WubiMaster/Controls/PagingControl.xaml.cs
@@ -0,0 +1,77 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WubiMaster.Controls
+{
+    public partial class PagingControl : UserControl
+    {
+        public static readonly DependencyProperty CountOfPageProperty =
+            DependencyProperty.Register("CountOfPage", typeof(int), typeof(PagingControl));
+
+        public static readonly DependencyProperty FirstPageCommandProperty =
+            DependencyProperty.Register("FirstPageCommand", typeof(RelayCommand<object>), typeof(PagingControl));
+
+        public static readonly DependencyProperty LastPageCommandProperty =
+            DependencyProperty.Register("LastPageCommand", typeof(RelayCommand<object>), typeof(PagingControl));
+
+        public static readonly DependencyProperty NextPageCommandProperty =
+            DependencyProperty.Register("NextPageCommand", typeof(RelayCommand<object>), typeof(PagingControl));
+
+        public static readonly DependencyProperty PageIndexProperty =
+                                    DependencyProperty.Register("PageIndex", typeof(int), typeof(PagingControl));
+
+        public static readonly DependencyProperty PreviousPageCommandProperty =
+            DependencyProperty.Register("PreviousPageCommand", typeof(RelayCommand<object>), typeof(PagingControl));
+
+        public static readonly DependencyProperty TotalPageCountProperty =
+                    DependencyProperty.Register("TotalPageCount", typeof(int), typeof(PagingControl));
+
+        public PagingControl()
+        {
+            InitializeComponent();
+        }
+
+        public int CountOfPage
+        {
+            get { return (int)GetValue(CountOfPageProperty); }
+            set { SetValue(CountOfPageProperty, value); }
+        }
+
+        public RelayCommand<object> FirstPageCommand
+        {
+            get { return (RelayCommand<object>)GetValue(FirstPageCommandProperty); }
+            set { SetValue(FirstPageCommandProperty, value); }
+        }
+
+        public RelayCommand<object> LastPageCommand
+        {
+            get { return (RelayCommand<object>)GetValue(LastPageCommandProperty); }
+            set { SetValue(LastPageCommandProperty, value); }
+        }
+
+        public RelayCommand<object> NextPageCommand
+        {
+            get { return (RelayCommand<object>)GetValue(NextPageCommandProperty); }
+            set { SetValue(NextPageCommandProperty, value); }
+        }
+
+        public int PageIndex
+        {
+            get { return (int)GetValue(PageIndexProperty); }
+            set { SetValue(PageIndexProperty, value); }
+        }
+
+        public RelayCommand<object> PreviousPageCommand
+        {
+            get { return (RelayCommand<object>)GetValue(PreviousPageCommandProperty); }
+            set { SetValue(PreviousPageCommandProperty, value); }
+        }
+
+        public int TotalPageCount
+        {
+            get { return (int)GetValue(TotalPageCountProperty); }
+            set { SetValue(TotalPageCountProperty, value); }
+        }
+    }
+}
