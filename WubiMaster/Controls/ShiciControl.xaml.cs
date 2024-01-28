09002b1cbd4bb3d225035f9646d5806615765da7
shiqiqi <mrshiqiqi@126.com>
2024-01-28 20:56:13 +0800

    今日诗词控件添加随机图片函数

--- WubiMaster/Controls/ShiciControl.xaml.cs
+++ WubiMaster/Controls/ShiciControl.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using WubiMaster.Common;
 using WubiMaster.Models;
 
@@ -12,22 +15,36 @@ namespace WubiMaster.Controls
         public static readonly DependencyProperty JinriShiciProperty =
             DependencyProperty.Register("JinriShici", typeof(string), typeof(ShiciControl));
 
+        public static readonly DependencyProperty ShiciImageProperty =
+            DependencyProperty.Register("ShiciImage", typeof(ImageSource), typeof(ShiciControl));
+
         public static readonly DependencyProperty WeatherProperty =
-            DependencyProperty.Register("Weather", typeof(string), typeof(ShiciControl));
+                    DependencyProperty.Register("Weather", typeof(string), typeof(ShiciControl));
 
         public ShiciControl()
         {
             InitializeComponent();
             httpRequestHelper = new HttpRequestHelper();
+            InitImages();
             GetToken();
             //GetWeather();
             GetJinrishici();
+
+            ShiciImage = ChangeImage();
         }
+
+        public List<ImageSource> Images { get; set; }
 
         public string JinriShici
         {
             get { return (string)GetValue(JinriShiciProperty); }
             set { SetValue(JinriShiciProperty, value); }
+        }
+
+        public ImageSource ShiciImage
+        {
+            get { return (ImageSource)GetValue(ShiciImageProperty); }
+            set { SetValue(ShiciImageProperty, value); }
         }
 
         public string Token { get; set; }
@@ -39,6 +56,13 @@ namespace WubiMaster.Controls
         }
 
         private HttpRequestHelper httpRequestHelper { get; set; }
+
+        private ImageSource ChangeImage()
+        {
+            Random random = new Random();
+            int index = random.Next(0, Images.Count);
+            return Images[index];
+        }
 
         private void GetJinrishici()
         {
@@ -86,6 +110,24 @@ namespace WubiMaster.Controls
             //Root model = JsonConvert.DeserializeObject<Root>(jsonString);
 
             //Weather = model.data.weatherData.weather;
+        }
+
+        private void InitImages()
+        {
+            Images = new List<ImageSource>();
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/侧脸.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/侧身微笑.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/低头.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/低头的人.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/翘二郎腿的女人.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/青花瓷少女.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/青花瓷衣服.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/手托头.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/思考.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/长发女人.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/正面.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/坐椅子的女人.png", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("../Images/JinriShici/坐着的女人.png", UriKind.Relative)));
         }
     }
 }
