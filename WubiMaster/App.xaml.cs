0d2f14b305761ec1d65702abae79295f248b2967
shiqiqi <sqqlove123@163.com>
2024-02-01 16:31:25 +0800

    添加全局异常捕获代码

--- WubiMaster/App.xaml.cs
+++ WubiMaster/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using WubiMaster.Common;
 
 namespace WubiMaster
@@ -16,11 +18,54 @@ namespace WubiMaster
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             LogHelper.Info("程序启动");
+
+            this.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
         }
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
             LogHelper.Info("程序退出");
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                e.Handled = true;
+                LogHelper.Fatal(e.Exception.Message);
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Fatal("程序发生致命错误，将终止，请联系开发人员！" + ex.Message);
+            }
+
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            StringBuilder sbEx = new StringBuilder();
+            if (e.IsTerminating)
+            {
+                sbEx.Append("程序发生致命错误，将终止，请联系开发人员！\n");
+            }
+
+            if (e.ExceptionObject is Exception)
+            {
+                sbEx.Append(((Exception)e.ExceptionObject).Message);
+            }
+            else
+            {
+                sbEx.Append(e.ExceptionObject);
+            }
+            MessageBox.Show(sbEx.ToString());
+        }
+
+        void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            LogHelper.Fatal(e.Exception.Message);
+            e.SetObserved();
         }
     }
 }
