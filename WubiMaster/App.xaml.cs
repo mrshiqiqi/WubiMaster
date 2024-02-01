43172d886e379cf995e49b73347f6bbb8a7ad2a0
shiqiqi <sqqlove123@163.com>
2024-02-01 16:48:51 +0800

    删除测试语句

--- WubiMaster/App.xaml.cs
+++ WubiMaster/App.xaml.cs
@@ -18,11 +18,6 @@ namespace WubiMaster
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             LogHelper.Info("程序启动");
-            LogHelper.Fatal("测试");
-            LogHelper.Debug("测试");
-            LogHelper.Info("信息");
-            LogHelper.Error("错误");
-            LogHelper.Warn("警告");
 
             this.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
             TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
