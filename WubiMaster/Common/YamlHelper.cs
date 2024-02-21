a551530021f72ab65c9121c5c12945f7c753ba70
shiqiqi <sqqlove123@163.com>
2024-02-21 18:15:42 +0800

    添加 YamlHelper

---
+++ WubiMaster/Common/YamlHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace WubiMaster.Common
+{
+    public static class YamlHelper
+    {
+        private static ISerializer _serializer;
+        private static IDeserializer _deserializer;
+
+        static YamlHelper()
+        {
+            _serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+            _deserializer = new DeserializerBuilder().WithNamingConvention(UnderscoredNamingConvention.Instance).Build();
+        }
+
+        public static string Serialize(object target)
+        {
+            return _serializer.Serialize(target);
+        }
+
+        public static void SerizlizerToFile(object target, string filePath)
+        {
+            var content = Serialize(target);
+            File.WriteAllText(filePath, content, Encoding.UTF8);
+        }
+
+        public static T Deserizlize<T>(string yaml)
+        {
+            return _deserializer.Deserialize<T>(yaml);
+        }
+
+        public static T DeserializeFromFile<T>(string filePath)
+        {
+            var yaml = File.ReadAllText(filePath, Encoding.UTF8);
+            return Deserizlize<T>(yaml);
+        }
+    }
+}
--- WubiMaster/ViewModels/LexiconViewModel.cs
+++ WubiMaster/ViewModels/LexiconViewModel.cs
@@ -2,10 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WubiMaster.Common;
 using WubiMaster.Models;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace WubiMaster.ViewModels
 {
@@ -13,6 +17,13 @@ namespace WubiMaster.ViewModels
     {
         [ObservableProperty]
         public ObservableCollection<CikuModel> cikuList;
+
+        private string cikuPath = ConfigHelper.ReadConfigByString("user_file_path")+ "\\wubi06_ci.dict.yaml";
+
+        private void ReadCikuData()
+        {
+
+        }
 
         public LexiconViewModel()
         {
