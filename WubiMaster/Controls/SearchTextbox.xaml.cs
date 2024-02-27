50947153bb8f9107eb3d905c57a2f31660c81817
shiqiqi <sqqlove123@163.com>
2024-02-27 17:09:17 +0800

    搜索框控件重构

--- WubiMaster/Controls/SearchTextbox.xaml.cs
+++ WubiMaster/Controls/SearchTextbox.xaml.cs
@@ -16,6 +16,74 @@ namespace WubiMaster.Controls
             InitializeComponent();
         }
 
+
+
+
+        public Thickness TextMargin
+        {
+            get { return (Thickness)GetValue(TextMarginProperty); }
+            set { SetValue(TextMarginProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextMarginProperty =
+            DependencyProperty.Register("TextMargin", typeof(Thickness), typeof(SearchTextbox));
+
+
+
+        public CornerRadius BorderCornerRadius
+        {
+            get { return (CornerRadius)GetValue(BorderCornerRadiusProperty); }
+            set { SetValue(BorderCornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty BorderCornerRadiusProperty =
+            DependencyProperty.Register("BorderCornerRadius", typeof(CornerRadius), typeof(SearchTextbox));
+
+
+
+
+        public double TextFontSize
+        {
+            get { return (double)GetValue(TextFontSizeProperty); }
+            set { SetValue(TextFontSizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextFontSizeProperty =
+            DependencyProperty.Register("TextFontSize", typeof(double), typeof(SearchTextbox));
+
+
+
+        public Visibility ShowSearchButton
+        {
+            get { return (Visibility)GetValue(ShowSearchButtonProperty); }
+            set { SetValue(ShowSearchButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowSearchButtonProperty =
+            DependencyProperty.Register("ShowSearchButton", typeof(Visibility), typeof(SearchTextbox));
+
+
+        public CornerRadius CombCornerRadius
+        {
+            get { return (CornerRadius)GetValue(CombCornerRadiusProperty); }
+            set { SetValue(CombCornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CombCornerRadiusProperty =
+            DependencyProperty.Register("CombCornerRadius", typeof(CornerRadius), typeof(SearchTextbox));
+
+
+
+        public Visibility ShowComboBox
+        {
+            get { return (Visibility)GetValue(ShowComboBoxProperty); }
+            set { SetValue(ShowComboBoxProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowComboBoxProperty =
+            DependencyProperty.Register("ShowComboBox", typeof(Visibility), typeof(SearchTextbox));
+
+
         public string SecrchContent
         {
             get { return (string)GetValue(SecrchContentProperty); }
