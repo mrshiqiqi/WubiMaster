18ea961050509073fba9ddb2b8651c877738e3b8
shiqiqi <sqqlove123@163.com>
2024-02-27 17:46:30 +0800

    调整控件配色

--- WubiMaster/Controls/SearchTextbox.xaml.cs
+++ WubiMaster/Controls/SearchTextbox.xaml.cs
@@ -5,30 +5,37 @@ namespace WubiMaster.Controls
 {
     public partial class SearchTextbox : UserControl
     {
+        public static readonly DependencyProperty BorderCornerRadiusProperty =
+            DependencyProperty.Register("BorderCornerRadius", typeof(CornerRadius), typeof(SearchTextbox));
+
+        public static readonly DependencyProperty CombCornerRadiusProperty =
+            DependencyProperty.Register("CombCornerRadius", typeof(CornerRadius), typeof(SearchTextbox));
+
         public static readonly DependencyProperty SecrchContentProperty =
-            DependencyProperty.Register("SecrchContent", typeof(string), typeof(SearchTextbox));
+                            DependencyProperty.Register("SecrchContent", typeof(string), typeof(SearchTextbox));
+
+        public static readonly DependencyProperty ShowComboBoxProperty =
+            DependencyProperty.Register("ShowComboBox", typeof(Visibility), typeof(SearchTextbox));
+
+        public static readonly DependencyProperty ShowSearchButtonProperty =
+            DependencyProperty.Register("ShowSearchButton", typeof(Visibility), typeof(SearchTextbox));
+
+        public static readonly DependencyProperty TextFontSizeProperty =
+            DependencyProperty.Register("TextFontSize", typeof(double), typeof(SearchTextbox));
+
+        public static readonly DependencyProperty TextMarginProperty =
+            DependencyProperty.Register("TextMargin", typeof(Thickness), typeof(SearchTextbox));
+
+        public static readonly DependencyProperty TextMaxLengthProperty =
+            DependencyProperty.Register("TextMaxLength", typeof(int), typeof(SearchTextbox), new PropertyMetadata(10));
 
         public static readonly DependencyProperty WaterMarkTextProperty =
-            DependencyProperty.Register("WaterMarkText", typeof(string), typeof(SearchTextbox));
+                                                    DependencyProperty.Register("WaterMarkText", typeof(string), typeof(SearchTextbox));
 
         public SearchTextbox()
         {
             InitializeComponent();
         }
-
-
-
-
-        public Thickness TextMargin
-        {
-            get { return (Thickness)GetValue(TextMarginProperty); }
-            set { SetValue(TextMarginProperty, value); }
-        }
-
-        public static readonly DependencyProperty TextMarginProperty =
-            DependencyProperty.Register("TextMargin", typeof(Thickness), typeof(SearchTextbox));
-
-
 
         public CornerRadius BorderCornerRadius
         {
@@ -36,43 +43,17 @@ namespace WubiMaster.Controls
             set { SetValue(BorderCornerRadiusProperty, value); }
         }
 
-        public static readonly DependencyProperty BorderCornerRadiusProperty =
-            DependencyProperty.Register("BorderCornerRadius", typeof(CornerRadius), typeof(SearchTextbox));
-
-
-
-
-        public double TextFontSize
-        {
-            get { return (double)GetValue(TextFontSizeProperty); }
-            set { SetValue(TextFontSizeProperty, value); }
-        }
-
-        public static readonly DependencyProperty TextFontSizeProperty =
-            DependencyProperty.Register("TextFontSize", typeof(double), typeof(SearchTextbox));
-
-
-
-        public Visibility ShowSearchButton
-        {
-            get { return (Visibility)GetValue(ShowSearchButtonProperty); }
-            set { SetValue(ShowSearchButtonProperty, value); }
-        }
-
-        public static readonly DependencyProperty ShowSearchButtonProperty =
-            DependencyProperty.Register("ShowSearchButton", typeof(Visibility), typeof(SearchTextbox));
-
-
         public CornerRadius CombCornerRadius
         {
             get { return (CornerRadius)GetValue(CombCornerRadiusProperty); }
             set { SetValue(CombCornerRadiusProperty, value); }
         }
 
-        public static readonly DependencyProperty CombCornerRadiusProperty =
-            DependencyProperty.Register("CombCornerRadius", typeof(CornerRadius), typeof(SearchTextbox));
-
-
+        public string SecrchContent
+        {
+            get { return (string)GetValue(SecrchContentProperty); }
+            set { SetValue(SecrchContentProperty, value); }
+        }
 
         public Visibility ShowComboBox
         {
@@ -80,14 +61,28 @@ namespace WubiMaster.Controls
             set { SetValue(ShowComboBoxProperty, value); }
         }
 
-        public static readonly DependencyProperty ShowComboBoxProperty =
-            DependencyProperty.Register("ShowComboBox", typeof(Visibility), typeof(SearchTextbox));
-
-
-        public string SecrchContent
+        public Visibility ShowSearchButton
         {
-            get { return (string)GetValue(SecrchContentProperty); }
-            set { SetValue(SecrchContentProperty, value); }
+            get { return (Visibility)GetValue(ShowSearchButtonProperty); }
+            set { SetValue(ShowSearchButtonProperty, value); }
+        }
+
+        public double TextFontSize
+        {
+            get { return (double)GetValue(TextFontSizeProperty); }
+            set { SetValue(TextFontSizeProperty, value); }
+        }
+
+        public Thickness TextMargin
+        {
+            get { return (Thickness)GetValue(TextMarginProperty); }
+            set { SetValue(TextMarginProperty, value); }
+        }
+
+        public int TextMaxLength
+        {
+            get { return (int)GetValue(TextMaxLengthProperty); }
+            set { SetValue(TextMaxLengthProperty, value); }
         }
 
         public string WaterMarkText
