2583bdc4c9d0916e168b12c6dc5733f350c9d694
shiqiqi <sqqlove123@163.com>
2024-03-28 18:24:03 +0800

    解决rime切换账户多进程无法被识别问题

--- WubiMaster/Common/ServiceHelper.cs
+++ WubiMaster/Common/ServiceHelper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
 
 namespace WubiMaster.Common
 {
@@ -11,23 +14,30 @@ namespace WubiMaster.Common
             string serverName = GlobalValues.ServerName;
             if (string.IsNullOrEmpty(serverName)) { return false; }
             Process[] ps = Process.GetProcessesByName(serverName.Split(".exe")[0]);
-            if (ps.Length > 0)
-            {
-                return true;
-            }
-            return false;
+            if (ps.Length <= 0)
+                return false;
+
+            var _p = ps.FirstOrDefault(p => GetProcessUserName(p.Id) == Environment.UserName);
+            if (_p == null) return false;
+            return true;
         }
 
         public static void KillService()
         {
-            if (!ServiceHelper.FindService()) return;
-            //string prcessPath = ConfigHelper.ReadConfigByString("process_file_path");
-            string processPath = GlobalValues.ProcessPath;
-            if (string.IsNullOrEmpty(processPath))
-            {
+            if (!ServiceHelper.FindService())
                 throw new Exception("请先配置程序文件目录");
-            }
-            CmdHelper.RunCmd(processPath, "WeaselServer.exe /q", false);
+
+            Process[] ps = Process.GetProcessesByName(GlobalValues.ServerName.Split(".exe")[0]);
+            var _p = ps.FirstOrDefault(p => GetProcessUserName(p.Id) == Environment.UserName);
+            if (_p != null) { _p.Kill(); }
+
+            //string prcessPath = ConfigHelper.ReadConfigByString("process_file_path");
+            //string processPath = GlobalValues.ProcessPath;
+            //if (string.IsNullOrEmpty(processPath) || !Directory.Exists(processPath))
+            //{
+            //    throw new Exception("请先配置程序文件目录");
+            //}
+            //CmdHelper.RunCmd(processPath, "WeaselServer.exe /q", false);
         }
 
         public static void RunService()
@@ -40,6 +50,38 @@ namespace WubiMaster.Common
                 throw new Exception("请先配置程序文件目录");
             }
             CmdHelper.RunCmd(processPath, "WeaselServer.exe", false);
+        }
+
+        /// <summary>
+        /// 查找进程所属的系统账户
+        /// </summary>
+        /// <param name="pID"></param>
+        /// <returns></returns>
+        private static string GetProcessUserName(int pID)
+        {
+            string userName = string.Empty;
+
+            try
+            {
+                foreach (ManagementObject item in new ManagementObjectSearcher("Select * from Win32_Process WHERE processID=" + pID).Get())
+                {
+                    ManagementBaseObject inPar = null;
+                    ManagementBaseObject outPar = null;
+
+                    inPar = item.GetMethodParameters("GetOwner");
+                    outPar = item.InvokeMethod("GetOwner", inPar, null);
+
+                    userName = Convert.ToString(outPar["User"]);
+
+                    break;
+                }
+            }
+            catch
+            {
+                userName = "SYSTEM";
+            }
+
+            return userName;
         }
     }
 }
