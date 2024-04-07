a33d386510d4a36d7e266f6d381b66ccd633cd3d
shiqiqi <sqqlove123@163.com>
2024-04-07 11:16:41 +0800

    添加了 TestViewModel

---
+++ WubiMaster/ViewModels/TestViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WubiMaster.ViewModels
+{
+    public partial class TestViewModel : ObservableRecipient
+    {
+        public TestViewModel()
+        {
+            //GetAiColorString();
+        }
+
+        private void GetAiColorString()
+        {
+            // 创建一个WebRequest实例(默认get方式)
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://aicolors.co");
+            //可以指定请求的类型
+            //request.Method = "POST";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            // 接收数据
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            // 关闭stream和response
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+        }
+    }
+}
